17c47ed2b6392eb1a116697916112beb9a541d83
auryvaldes <47179470+auryvaldes@users.noreply.github.com>
2021-09-10 17:09:09 -0300

    coments del codigo

--- meli-aury/routes/items.js
+++ meli-aury/routes/items.js
@@ -5,14 +5,17 @@ const {
 } = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* como se me pidio cree un servicio con express(nodejs), donde lo primero que hago a traves de axio es consumir la url de la query propocionada */
 router.get('/', async function (req, resp, next) {
   try {
     console.log(req.query.query);
+    //para este caso hago la colsulta de la query a traves de query strings
     const res = await axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${req.query.query}`);
     if (!res) {
       console.error(`Ha ocurrido un error en la llamada`);
     }
+
+    //los datos del autor no eran dados por lo que cree un obejeto con la informacion hardcodeada
     const response = {
       author: {
         name: 'Aury',
@@ -25,6 +28,7 @@ router.get('/', async function (req, resp, next) {
       const categories = res.data.available_filters.find(f => f.id === 'category');
       if (categories && categories.values) response.categories = categories.values.map(c => c.name);
     }
+    // con la respuesta obtenida de la consulta armo el json para extraer los datos necesarios para el armado de las pantallas
     if (res.data && res.data.results) {
       let items = res.data.results.map(r => {
         return {
@@ -50,7 +54,7 @@ router.get('/', async function (req, resp, next) {
   }
 
 });
-
+// en este caso reaizo el mismo tratamiento de la api pero con la salvedad que hago una consulta de url parameters 
 router.get('/:id', async function (req, res, next) {
   try {
     console.log(req.params.id);
--- meli-aury/src/Components/productsPage.js
+++ meli-aury/src/Components/productsPage.js
@@ -1,3 +1,5 @@
+//importacion de las librerias
+
 import React, { useState } from 'react';
 
 import { BiSearchAlt2 } from 'react-icons/bi';
@@ -8,13 +10,16 @@ import shipping from '../image/ic_shipping.png'
 
 
 const ProductsPage = () => {
+  //declaracion de las variables de style
   const { searchBar, WrappContainer, logoMeli, lupaSearch, productsList_ul,
     productsList_li, productsList_img, WrappContainerSeacr, WrappContainer_ul, 
     img_shipping, productsList_state } = styles;
 
+    // declaracacion de la variable y las fusiones que me permiten obtener la lista de productos
   const [persistFound, setpersistFound] = useState('');
   const [foundproducts, setFoundproducts] = useState();
 
+//fucion que optiene los datos de la api y actuliza al componente  a traves del useState
   const ApiCall = (value) => {
     let params = value ? `?query=${value}` : ''
     fetch(`/api/items${params}`)
@@ -30,9 +35,13 @@ const ProductsPage = () => {
       )
   }
   return (
+    /* WrappContainer de la primera pantallas:
+    Se encuentra la seacrhBar con sus metetodos de invocacion a la api, no logre rerpducir de manera limpia que se llama a traves del enter
+    por que lo que decidí dejar el llamado solo desde el clik en la lupa.
+    luego tenemos el result de la lista de productos recibidos despues del envio del parametro introducido en en input*/
     <div style={WrappContainer}>
       <div style={WrappContainerSeacr}>
-        <img style={logoMeli} src={logo} alt='' />
+        <img style={logoMeli} src={logo} alt='' /> 
         <input style={searchBar}
           type='search'
           value={persistFound}
@@ -41,6 +50,7 @@ const ProductsPage = () => {
           placeholder='Nunca dejes de buscar' />
         <BiSearchAlt2 onClick={() => ApiCall(persistFound)} style={lupaSearch} />
       </div>
+      
       <div style={WrappContainer_ul}>
         {foundproducts && foundproducts.items.length > 0 ? (
           foundproducts.items.map((prod => (
@@ -69,6 +79,7 @@ const ProductsPage = () => {
   );
 };
 
+//armado de los styles
 const styles = {
   WrappContainer: {
     backgroundColor: '#EEEEEE',
